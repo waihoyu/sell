15dcd422bb4a3c8155299e852dac0805a9e1445a
waihoyu <13701343809@qq.com>
2018-06-27 13:56:41 +0800

    完成头部子组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -42,6 +42,9 @@ export default {
         font-size 0
         .avatar
             display inline-block
+            vertical-align top
+            img
+                border-radius 2px
         .content
             display inline-block
             font-size 14px
@@ -60,4 +63,16 @@ export default {
                     margin-left 6px
                     font-size 16px
                     line-height 18px
+            .description
+                margin-bottom 10px
+                line-height 12px
+                font-size 12px
+            .support
+                .icon
+                    display inline-block
+                    width 12px
+                    height 12px
+                    margin-right 4px
+                    background-size 12px 12px
+                    background-repeat no-repeat
 </style>
