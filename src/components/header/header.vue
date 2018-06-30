17a6f6e6a535a7dc78cead7d29e1aedfc219f859
waihoyu <13701343809@qq.com>
2018-06-30 21:33:16 +0800

    finish the star-level modual

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
     <div class="header">
         <div class="content-wrapper">
@@ -12,7 +13,7 @@
                 <div class="description">
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
-                <div v-if="seller.supports" class="support">
+                <div v-if="seller.supports" class="support" @click="showDetail">
                     <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
@@ -22,24 +23,51 @@
                 <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
-        <div class="bulletin-wrapper">
+        <div class="bulletin-wrapper" @click="showDetail">
             <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span><i class="icon-keyboard_arrow_right"></i>
         </div>
         <div class="background">
-            <img :src="seller.avatar" width=100% height= 100% alt="" srcset="">
+            <img :src="seller.avatar" width=100% height=100% alt="" srcset="">
+        </div>
+        <div v-show="detailShow" class="detail">
+            <div class="detail-wrapper clearfix">
+                <div class="detail-main">
+                    <h1 class="name">{{seller.name}}</h1>
+                    <div class="star-wrapper">
+                        <star :size="48" :score="seller.score"></star>
+                    </div>
+                </div>
+            </div>
+            <div class="detail-close">
+                <i class="icon-close"></i>
+            </div>
         </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+    import star from '../star/star'
     export default {
         props: {
             seller: {
                 type: Object
             }
         },
+        data() {
+            return {
+                detailShow: false
+            }
+        },
+        methods: {
+            showDetail() {
+                this.detailShow = true
+            }
+        },
         created() {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+        },
+        components: {
+            star
         }
     }
 </script>
@@ -48,6 +76,7 @@
 @import '../../common/stylus/mixin'
 .header
     position relative
+    overflow hidden
     color #fff
     background rgba(7, 17, 27, 0.5)
     .content-wrapper
@@ -134,7 +163,7 @@
         .bulletin-title
             display inline-block
             vertical-align top
-            margin-top 7px
+            margin-top 8px
             width 22px
             height 12px
             bg-image('bulletin')
@@ -158,4 +187,35 @@
         height 100%
         z-index -1
         filter blur(10px)
+    .detail
+        position fixed
+        top 0
+        left 0
+        z-index 100
+        width 100%
+        height 100%
+        overflow auto
+        background rgba(7,17,27,0.8)
+        .detail-wrapper
+            min-height 100%
+            width 100%
+            .detail-main
+                margin-top: 64px
+                padding-bottom: 64px
+                .name
+                    line-height 16px
+                    text-align center
+                    font-size 16px
+                    font-weight 700
+                .star-wrapper
+                    margin-top 18px
+                    padding 2px 0
+                    text-align center
+        .detail-close
+            position relative
+            width 32px
+            height 32px
+            margin -64px auto 0 auto
+            clear both
+            font-size 32px
 </style>
