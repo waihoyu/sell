2a48cdf539af02bd29e11932352211725a425058
waihoyu <13701343809@qq.com>
2018-07-08 23:15:19 +0800

    update the layout of the right-food-list

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -14,9 +14,9 @@
                 <li v-for="item in goods" class="food-list">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
-                        <li v-for="food in item.foods" class="food-item">
+                        <li v-for="food in item.foods" class="food-item border-1px">
                             <div class="icon">
-                                <img :src="food.icon" alt="">
+                                <img width="57" height="57" :src="food.icon" alt="">
                             </div>
                             <div class="content">
                                 <h2 class="name">{{food.name}}</h2>
@@ -110,6 +110,34 @@
                 border-1px(rgba(7,17,27,0.1))
                 width 56px
 
-    .foods.wrapper
+    .foods-wrapper
         flex 1
+        .title
+            padding-left 14px
+            height 26px
+            line-height 26px
+            border-left 2px solid #d9dde1
+            font-size 12px
+            color rgb(147,153,159)
+            background #f3f5f7
+        .food-item
+            display flex
+            margin 18px
+            padding-bottom 18px
+            border-1px(rgba(7,17,27,0.1))
+            &:last-child
+                border-none()
+                margin-bottom 0
+            .icon
+                flex 0 0 57px
+                margin-right 10px
+            .content
+                flex 1
+                .name
+                    margin 2px 0 8px 0
+                    line-height 14px
+                    height 14px
+                    font-size 14px
+                    color rgb(7,17,27)
+                    
 </style>
