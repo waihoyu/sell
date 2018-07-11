e346da30bc068b67911706c73a09f6caf5feeaae
waihoyu <13701343809@qq.com>
2018-07-11 14:07:40 +0800

    finish the better-scroll module

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="goods">
+
         <div class="menu-wrapper" ref = "menuWrapper">
             <ul>
-                <li v-for="item in goods" class="menu-item" :key="item">
+                <li v-for = "(item, index) in goods" class = "menu-item" :class="{'current':currentIndex === index}" :key= "index" @click="selectMenu(index, $event)" >
                     <span class="text border-1px">
                         <span v-show="item.type >0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
                     </span>
@@ -12,10 +13,10 @@
         
         <div class="foods-wrapper" ref = "foodsWrapper">
             <ul>
-                <li v-for="item in goods" class="food-list" :key="item">
+                <li v-for="(item, index) in goods" class="food-list food-list-hook" :key="index">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
-                        <li v-for="food in item.foods" class="food-item border-1px" :key="food">
+                        <li v-for="(food, index) in item.foods" class="food-item border-1px" :key="index">
                             <div class="icon">
                                 <img width="57" height="57" :src="food.icon" alt="">
                             </div>
@@ -49,8 +50,22 @@
         },
         data(){
             return {
-                goods:[]
+                goods: [],
+                listHeight: [],
+                scrollY: 0
             } 
+        },
+        computed: {
+            currentIndex (){
+                for (let i = 0; i < this.listHeight.length; i++) {
+                      let height1 = this.listHeight[i]
+                      let height2 = this.listHeight[i + 1]
+                      if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                          return i
+                      }      
+                }
+                return 0         
+            }
         },
         created(){
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -60,15 +75,41 @@
                 this.goods = response.data
                 this.$nextTick(() => {
                      this._initScroll ()
+                     this._calculateHeight()
                 })
-                this.seller = Object.assign({}, this.seller, response.data)
+                // this.seller = Object.assign({}, this.seller, response.data)
             }
             })
         },
         methods: {
+            selectMenu (index, event) {
+                if (!event._constructed){
+                    return
+                }
+                let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+                let el = foodList[index]
+                this.foodsScroll.scrollToElement(el, 300)
+            },
             _initScroll () {
-                this.menuScroll = new BScroll(this.$refs.menuWrapper,{})
-                this.foodScroll = new BScroll(this.$refs.foodsWrapper,{})
+                this.menuScroll = new BScroll(this.$refs.menuWrapper,{
+                    click: true
+                })
+                this.foodsScroll = new BScroll(this.$refs.foodsWrapper,{
+                    probeType: 3
+                })
+                this.foodsScroll.on('scroll', (pos) => {
+                    this.scrollY = Math.abs(Math.round(pos.y))
+                })
+            },
+            _calculateHeight () {
+                let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+                var height = 0
+                this.listHeight.push(height)
+                for (let i = 0; i < foodList.length; i++) {
+                    let item = foodList[i]
+                    height += item.clientHeight
+                    this.listHeight.push(height)
+                }
             }
         }
     }
@@ -93,6 +134,15 @@
             width 56px
             line-height 14px
             padding 0 12px
+            &.current
+                position relative
+                z-index 10
+                margin-top -1px
+                background #fff
+                font-weight 700
+                .text
+                    borde-none()
+
             .icon
                 display inline-block
                 vertical-align top
