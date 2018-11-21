dbc23c14a528564129408fb22f97af802c7eac97
waihoyu <13701343809@qq.com>
2018-11-21 09:57:42 +0800

    update

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        ratings
+    <div class="ratings">
+      <div class="ratings-content">
+1
+      </div>
     </div>
 </template>
 
@@ -8,5 +10,12 @@
 export default {}
 </script>
 
-<style lang="stylus" res="stylesheet/stylus">
+<style lang="stylus" res="stylesheet/stylus" type="text/less(stylus)">
+ /*.ratings*/
+    /*position: absolute*/
+    /*top: 174px*/
+    /*bottom: 0*/
+    /*left: 0*/
+    /*width: 100%*/
+    /*overflow: hidden*/
 </style>
