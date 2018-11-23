227bf4cec8a83e5e4fd74e79974a0d8cceddbe00
waihoyu <13701343809@qq.com>
2018-11-23 23:52:44 +0800

    ratings.vue update

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,34 +1,34 @@
 
 <template>
-    <div class="ratings">
-      <div class="ratings-content">
-          <div class="overview">
-            <div class="overview-left">
-              <h1 class="score">
-                {{seller.score}}
-              </h1>
-              <div class="title">综合评分</div>
-              <div class="rank">高于周边商家{{seller.rankRate}}%</div>
-            </div>
-            <div class="overview-right">
-              <div class="score-wrapper">
-                <span class="title">服务态度</span>
-                <star :size="36" :score="seller.serviceScore" ></star>
-                <span class="score">{{seller.serviceScore}}</span>
-              </div>
-              <div class="score-wrapper">
-                <span class="title">商品评分</span>
-                <star :size="36" :score="seller.foodScore"></star>
-                <span class="score">{{seller.foodScore}}</span>
-              </div>
-              <div class="delivery-wrapper">
-                <span class="title">送达时间</span>
-                <span class="score">{{seller.deliveryTime}}</span>
-              </div>
-            </div>
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">
+            {{seller.score}}
+          </h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore" ></star>
+            <span class="score">{{seller.serviceScore}}</span>
           </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script type="text/javascript">
@@ -46,42 +46,76 @@
 </script>
 
 <style lang="stylus" res="stylesheet/stylus" type="text/stylus">
-  .ratings
-    position absolute
-    top 174px
-    bottom 0
-    left 0
-    width 100%
-    overflow hidden
-    .overview
-      background red
-      display flex
-      padding 18px 0
-    .overview-left
-      background #00d6b2
-      flex 0 0 137px
-      width 137px
-      border-right 1px solid rgba(7, 17,27,0.9)
-      text-align center
-      padding 6px 0
-      .score
-        background yellow
-        margin-bottom 6px
-        font-size 24px
-        line-height 28px
-        color rgb(255,153,0)
-      .title
-        background #0b97c4
-        margin-bottom 8px
-        line-height 12px
-        font-size 12px
-        color rgb(7, 17, 27)
-      .rank
-        line-height: 10px
-        font-size 10px
-        color rgb(147,153,159)
-    .overview-right
-      background #00b3ee
-      flex 1
-      padding-left:24px
+    @import "../../common/stylus/mixin.styl"
+    .ratings
+        position absolute
+        top 174px
+        bottom 0
+        left 0
+        width 100%
+        overflow hidden
+        .overview
+            background red
+            display flex
+            padding 18px 0
+            .overview-left
+                background #00d6b2
+                flex 0 0 137px
+                width 137px
+                border-right 1px solid rgba(7, 17,27,0.9)
+                text-align center
+                padding 6px 0
+                @media only screen and  (max-width 320px)
+                    flex 0 0 120px
+                    width 120px
+                .score
+                    background yellow
+                    margin-bottom 6px
+                    font-size 24px
+                    line-height 28px
+                    color rgb(255,153,0)
+                .title
+                    background #0b97c4
+                    margin-bottom 8px
+                    line-height 12px
+                    font-size 12px
+                    color rgb(7, 17, 27)
+                .rank
+                    line-height: 10px
+                    font-size 10px
+                    color rgb(147,153,159)
+            .overview-right
+                background #00b3ee
+                flex 1
+                padding 6px 0 6px 24px
+                @media only screen and (max-width 320px)
+                    padding-left 6px
+                .score-wrapper
+                    margin-bottom 8px
+                    font-size 0
+                    .title
+                        vertical-align top
+                        font-size 12px
+                        color rgb(7,17,27)
+                        line-height 18px
+                    .star
+                        display: inline-block
+                        margin 0 12px
+                        vertical-align top
+                    .score
+                        display inline-block
+                        vertical-align top
+                        font-size 12px
+                        line-height 18px
+                        color rgb(255,153,0)
+                .delivery-wrapper
+                    font-size 0
+                    .title
+                        font-size 12px
+                        color rgb(7,17,27)
+                        line-height 18px
+                    .delivery
+                        margin-left: 12px
+                        font-size: 12px
+                        color: rgb(147, 153, 159)
 </style>
